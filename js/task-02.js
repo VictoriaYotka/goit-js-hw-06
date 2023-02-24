24cc603fe4fe3f429471f87339e1a68df3b963ec
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-02-24 18:11:18 +0200

    task-2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const list = document.querySelector('#ingredients');
+
+const createItemsArray = array => {
+  return array.map(el => {
+const item = document.createElement('li');
+item.textContent = el;
+item.classList.add('item');
+return item})
+};
+
+const newList = createItemsArray(ingredients);
+
+list.append(...newList);
+
+
