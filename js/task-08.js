4055da5ed56f55c0e21798c17c268aa7c514e798
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-02-26 16:31:25 +0200

    task-8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector('.login-form');
 
+const handlerFormSubmit = (event) => {
+    event.preventDefault();
+
+    const {elements: {email, password}} = event.currentTarget;
+  
+    if(email.value === '' || password.value === '') {
+        alert('Заповніть усі поля!');
+        return
+    }
+
+    const userData = {
+    'email': email.value,
+    'password': password.value,
+    }
+
+    console.log(userData);
+
+    event.currentTarget.reset();
+};
+
+
+formEl.addEventListener('submit', handlerFormSubmit)
