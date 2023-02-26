e8643f35820ee44e99b4f73e6d2fc6aebe796100
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-02-26 15:20:17 +0200

    task-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.body;
+const colorNameEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color');
+
+const handlerBodyColorChange = () => {
+  colorNameEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = colorNameEl.textContent;
+  
+};
+
+buttonEl.addEventListener('click', handlerBodyColorChange);
