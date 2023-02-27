bb731fde61453f4cc2b1acf0adc15f0af65ca34e
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-02-27 12:16:17 +0200

    improve hw

--- js/task-05.js
+++ js/task-05.js
@@ -2,10 +2,10 @@ const inputEl = document.querySelector('#name-input');
 const outputEl = document.querySelector('#name-output');
 
 const handlerInputTextToOutput = () => {
-    if(inputEl.value.length === 0) {
+    if(inputEl.value.length === 0 || inputEl.value.trim() === '') {
 return outputEl.textContent = "Anonymous";
     }
-    return outputEl.textContent = inputEl.value;
+    return outputEl.textContent = inputEl.value.trim();
 };
 
 inputEl.addEventListener('input', handlerInputTextToOutput);
--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,11 @@
 const inputEl = document.querySelector('#validation-input');
 
 const handlerInputValidation = () => {
-    if(inputEl.value.length < inputEl.dataset.length) {
-        inputEl.classList.add('invalid');
-        return;
+    if(inputEl.value.length == inputEl.dataset.length) {  
+        return inputEl.classList.add('valid');
     }
-
-    if(inputEl.value.length >= inputEl.dataset.length) {
-        inputEl.classList.add('valid');
-        return;
-    }  
+    
+       return inputEl.classList.add('invalid'); 
 };
 
 inputEl.addEventListener('blur', handlerInputValidation);
